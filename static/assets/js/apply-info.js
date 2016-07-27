a7be3512a26b9f7262c2bc1f8af2de8f5762711e
RedbackThomson <redbackthomson@gmail.com>
2016-07-27 14:30:23 +0800

    Autocomplete width fix

--- static/assets/js/apply-info.js
+++ static/assets/js/apply-info.js
@@ -77,4 +77,9 @@ $(document).ready(function() {
       processData: false
     });
   });
+
+  jQuery.ui.autocomplete.prototype._resizeMenu = function () {
+    var ul = this.menu.element;
+    ul.outerWidth(this.element.outerWidth());
+  };
 });
