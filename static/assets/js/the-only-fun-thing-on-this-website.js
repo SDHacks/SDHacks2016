e3eedd237a1bded3547d8a8cb6bb9adc387a4449
RedbackThomson <redbackthomson@gmail.com>
2016-08-03 14:22:19 +0800

    Allow University to be an open field

--- static/assets/js/the-only-fun-thing-on-this-website.js
+++ static/assets/js/the-only-fun-thing-on-this-website.js
@@ -32,5 +32,6 @@ $(document).ready(function() {
     '              `:;;:,              '];
   var logoString = logo.join('\n');
   console.log('%cWelcome to SD Hacks 2016!', 'font: "Source Sans Pro","Opens Sans",Helvetica,Roboto,Arial,sans-serif; color: #19A3C5; font-size: 1.5em;');
-  console.log('%c' + logoString, 'font: 20px/10px monospace');
+  console.log('%cThis website is open-sourced on Github at %chttps://github.com/SDHacks/SDHacks2016', 'font: "Source Sans Pro","Opens Sans",Helvetica,Roboto,Arial,sans-serif; font-size: 1em;', 'color: #19A3C5');
+  console.log('%c' + logoString, 'font: 20px/10px monospace;');
 });
